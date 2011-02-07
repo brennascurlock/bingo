3b3b2b11435518ca32da6d031e07dcff1d46a2a9
Brenna Scurlock <brennascurlock@gmail.com>
2011-02-07 17:51:40 -0600

    Extracted navigation from home page, created navigation.php and inserted appropriate php commands on home page.

---
+++ navigation.php
@@ -0,0 +1,10 @@
+<nav>
+	<ul>
+		<li<?php if ($thisPage=="Home") 
+                echo " id=\"currentpage\""; ?>><a href="">Home</a></li>
+		<li<?php if ($thisPage=="Leaderboard") 
+                echo " id=\"currentpage\""; ?>><a href="">Leaderboard</a></li>
+		<li<?php if ($thisPage=="Restaurants") 
+                echo " id=\"currentpage\""; ?>><a href="">Restaurants</a></li>
+	</ul>
+</nav>
